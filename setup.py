80390dc1d2e832b3a711b85944341424ded1cd50
Jane Curry <jane.curry@skills-1st.co.uk>
2012-09-04 20:38:29 +0100

    ZenPacks.skills1st.MenuExamples 2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.skills1st.MenuExamples"
-VERSION = "1.0.3"
+VERSION = "2.0"
 AUTHOR = "Jane Curry"
 LICENSE = "GPLv2"
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.skills1st']
